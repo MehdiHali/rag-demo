93e869492d3baa1c143e79a55c2331b84b1f6244
Mehdi Ouled-hali <mehdi.ouledhali@gmail.com>
2025-01-14 19:26:39 +0100

    Initial commit

--- src/main/java/com/rag/demo_app/ChatController.java
+++ src/main/java/com/rag/demo_app/ChatController.java
@@ -2,28 +2,103 @@ package com.rag.demo_app;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingOptions;
+import org.springframework.ai.embedding.EmbeddingRequest;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class ChatController {
 
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
+    private final OllamaEmbeddingModel embeddingModel;
 
-    public ChatController(ChatClient.Builder chatClient, VectorStore vectorStore) {
+    @Value("classpath:/docs/java-interview-questions.pdf")
+    private Resource questionsPDF;
+
+    public ChatController(ChatClient.Builder chatClient, VectorStore vectorStore, OllamaEmbeddingModel embeddingModel) {
         this.chatClient = chatClient
                 .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
         this.vectorStore = vectorStore;
+        this.embeddingModel = embeddingModel;
     }
 
     @GetMapping("/")
     public String chat() {
-        return chatClient.prompt().user("From the context I've given you, is it possible to invest directly in an index?")
+        String l_prompt = "What are the top 4 ideas?";
+        System.out.println("CHAT CLIENT WILL ANSWER PROMPT: "+l_prompt);
+        return chatClient.prompt().user(l_prompt)
                 .call()
                 .content();
+    }
+
+    @GetMapping("/test1")
+    public float[] test1(){
+        String text = "hello";
+        float[] embeddings = embeddingModel.call(
+            new EmbeddingRequest(List.of(text), OllamaOptions.builder()
+              .withModel(OllamaModel.NOMIC_EMBED_TEXT) // why this is working and result doesn't change if i use all-minilm
+              .build()))
+          .getResult().getOutput();
+
+        float[] embeddiings2 = embeddingModel.embed(text);
+
+        return embeddings;
+    }
+
+    @GetMapping("/test2")
+    public float[] test2(){
+        String text = "hello";
+        float[] embeddings = embeddingModel.embed(text);
+
+        return embeddings;
+    }
+
+    @GetMapping("/split")
+    public List<Document> readPDF(){
+        TokenTextSplitter textSplitter = new TokenTextSplitter();
+
+        DocumentReader pdfReader = new PagePdfDocumentReader( questionsPDF, PdfDocumentReaderConfig.builder().withPageTopMargin(10).build());
+        List<Document> documents = pdfReader.read();
+        System.out.println("NUMBER OF DOCUMENTS BEFORE AND AFTER SPLITTING: "+ documents.size() + " SPLITED -> "+ textSplitter.split(documents).size());
+        return documents.subList(10,documents.size());
+    }
+
+    @GetMapping("/test3")
+    public Map<String, String> test3(){
+        Map<String,String> listOfEmbeddings = new HashMap<>();
+        int counter = 0;
+        List<Document> docs = readPDF();
+        docs = docs.subList(0,3);
+        docs.forEach(doc -> {
+            System.out.println("... Calculating: "+doc.getMetadata());
+            float[] l_embedding = embeddingModel.embed(doc.getContent());
+            float[] first10Embeddings = new float[10];
+            for(int i = 0 ; i < 10; i++){
+                first10Embeddings[i] = l_embedding[i];
+            }
+            System.out.println("Embedding: "+ Arrays.toString(l_embedding));
+            listOfEmbeddings.put(doc.getContent(), Arrays.toString(l_embedding));
+        });
+
+        return listOfEmbeddings;
     }
 
 }
--- src/main/java/com/rag/demo_app/InitializationService.java
+++ src/main/java/com/rag/demo_app/InitializationService.java
@@ -2,7 +2,11 @@ package com.rag.demo_app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -11,39 +15,40 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 /** LEARNING
  * This annotation tells spring boot this is a custom bean
  * that we want you to automatically instantiate and manage
   */
 @Component
-public class IngestionService implements CommandLineRunner {
+public class InitializationService implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger ( IngestionService.class );
+    private static final Logger log = LoggerFactory.getLogger ( InitializationService.class );
     private final VectorStore vectorStore;
 
-    public IngestionService(VectorStore vectorStore) {
+    public InitializationService(VectorStore vectorStore) {
         this.vectorStore = vectorStore;
     }
 
     /** LEARNING
      *
      */
-    //@Value("classpath:/docs/spring_boot_interview_questions.pdf")
-    //@Value("classpath:/docs/java-interview-questions.pdf")
-    @Value("classpath:/docs/article_thebeatoct2024.pdf")
+    @Value("classpath:/docs/java-interview-questions.pdf")
     private Resource questionsPDF;
 
     @Override
     public void run(String... args) throws Exception {
-        /** LEARNING
-         * The below code does everything, from reading the document
-         * to chuncking to embedding and loading to the vector store
-         */
+        TokenTextSplitter textSplitter = new TokenTextSplitter();
 
-        var pdfReader = new ParagraphPdfDocumentReader( questionsPDF );
-        TextSplitter textSplitter = new TokenTextSplitter();
-        vectorStore.accept(textSplitter.apply(pdfReader.get()));
+        // EXTRACT (READ)
+        DocumentReader pdfReader = new PagePdfDocumentReader( questionsPDF);
+        List<Document> documents = pdfReader.read();
+        // TRANSFORM (SPLIT)
+        List<Document>  chunks = textSplitter.split(pdfReader.get());
+        // LOAD (WRITE)
+        vectorStore.write(chunks);
         log.info("VectorStore loaded with data");
     }
 }
